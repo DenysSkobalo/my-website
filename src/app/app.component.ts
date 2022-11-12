837dd703f3bb7c4ac0a75fece4575c86b1877d35
DenysSkobalo <DenysSkobalo>
2022-11-12 22:39:28 +0100

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {GithubService} from "./services/github.service";
+import {IDataGithub} from "./models/data";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-blog';
+export class AppComponent implements OnInit{
+   @Input() data: IDataGithub | any
+
+
+  constructor(private githubService: GithubService) {
+  }
+
+  ngOnInit(): void {
+    this.githubService.getAll().subscribe(data => {
+      console.log(data)
+      this.data = data
+    })
+  }
 }
+
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IDataGithub} from "../models/data";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<IDataGithub> {
+    return this.http.get<IDataGithub>('https://api.github.com/users/DenysSkobalo');
+  }
+
+}
